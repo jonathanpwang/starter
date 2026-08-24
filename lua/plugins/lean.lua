af94ee8c9afa644faab6661e2da68834d5d5fd94
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2026-08-24 19:19:17 +0000

    feat: lean

---
+++ lua/plugins/lean.lua
@@ -0,0 +1,12 @@
+return {
+  "Julian/lean.nvim",
+  ft = { "lean" },
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    mappings = true,
+  },
+}
+
