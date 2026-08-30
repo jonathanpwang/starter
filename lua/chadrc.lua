36b300898784349470e0f6eb7950532f13901a85
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2026-08-30 15:15:19 -0700

    feat: full relative path in statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -14,11 +14,34 @@ M.base46 = {
 	-- },
 }
 
+M.ui = {
+	statusline = {
+		modules = {
+			-- show the file's path relative to cwd instead of just its name
+			file = function()
+				local stl_utils = require "nvchad.stl.utils"
+				local bufnr = stl_utils.stbufnr()
+				local path = vim.api.nvim_buf_get_name(bufnr)
+
+				local name, icon = "Empty", "󰈚"
+
+				if path ~= "" then
+					name = vim.fn.fnamemodify(path, ":.")
+
+					local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+					if devicons_present then
+						local ft_icon = devicons.get_icon(vim.fn.fnamemodify(path, ":t"))
+						icon = ft_icon or icon
+					end
+				end
+
+				local sep_r = stl_utils.separators.default.right
+				return "%#St_file# " .. icon .. " " .. name .. " %#St_file_sep#" .. sep_r
+			end,
+		},
+	},
+}
+
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
--- }
 
 return M
